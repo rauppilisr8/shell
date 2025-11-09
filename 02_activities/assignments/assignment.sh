fd2149f122f58cbab9817fd24dcf83c76f803aa5
rauppilisr8 <rauppilisr8@users.noreply.github.com>
2025-11-08 23:27:10 -0500

    v5.7

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -32,8 +32,8 @@ unzip -q rawdata.zip
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
 mkdir ./data/raw/
-ls ./data
-mv ./rawdata/* ./data/raw/
+ls
+mv -v ./rawdata/* ./data/raw/
 # 3. List the contents of the ./data/raw directory
 ls -l ./data/raw/
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
